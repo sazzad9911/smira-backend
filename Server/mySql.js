41a670b896f128344b86683e581c32ea3c351aa2
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-05-01 03:11:39 +0600

    ok

--- Server/mySql.js
+++ Server/mySql.js
@@ -1,18 +1,16 @@
 
-var mysql=require('mysql2');
+var mysql = require('mysql2');
 
-var pool = mysql.createPool({
-    host: "localhost",
-    user: "smira",
-    password: "S1245#%cdfSD@",
-    database: "simira"
-})
+module.exports = function () {
+    var pool = mysql.createPool({
+        host: "localhost",
+        user: "smira",
+        password: "S1245#%cdfSD@",
+        database: "simira"
+    })
 
-module.exports=function(){
-
-    
-    pool.getConnection((err, connection)=>{
-        if(err) {
+    pool.getConnection((err, connection) => {
+        if (err) {
             console.log(err);
             return;
         }
