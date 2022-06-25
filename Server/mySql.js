fd62997a9feee4f57d0a039159e060c347f4b7e4
Sazzad Hossain <sazzad15-2521@diu.edu.bd>
2022-06-26 02:42:47 +0600

    ok

--- Server/mySql.js
+++ Server/mySql.js
@@ -1,8 +1,8 @@
-var mysql = require('mysql');
+var mysql = require('mysql2');
 const pool = mysql.createPool({
     host: "localhost",
-    user: "root",
-    password: "",
+    user: "smira",
+    password: "S1245#%cdfSD@",
     database: "simira",
     connectionLimit : 1000,               // this is the max number of connections before your pool starts waiting for a release
     multipleStatements : true 
